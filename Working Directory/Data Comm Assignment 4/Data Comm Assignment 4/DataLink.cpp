357159795d5715c8806b1511ec8c99502f6e8d7a
jbayntun <j_bayntun@hotmail.com>
2014-11-26 20:03:04 -0800

    Added crc and packet creation to visual studio

---
+++ Working Directory/Data Comm Assignment 4/Data Comm Assignment 4/DataLink.cpp
@@ -0,0 +1,172 @@
+/*------------------------------------------------------------------------------------------------------------------
+-- SOURCE FILE: DataLink.cpp -
+--
+-- PROGRAM: Irregardless
+--
+-- FUNCTIONS:
+--		
+--
+-- DATE: November 26, 2014
+--
+-- REVISIONS: (Date and Description)
+--
+-- DESIGNER: Jeff Bayntun
+--
+-- PROGRAMMER: Jeff Bayntun
+--
+-- NOTES:
+--
+----------------------------------------------------------------------------------------------------------------------*/
+
+#include "DataLink.h"
+
+/*------------------------------------------------------------------------------------------------------------------
+-- FUNCTION: construcPacket
+--
+-- DATE: November 14, 2014
+--
+-- REVISIONS: (Date and Description)
+--
+-- DESIGNER: Jeff Bayntun
+--
+-- PROGRAMMER: Jeff Bayntun
+--
+-- INTERFACE: void constructPacket( unsigned char* packet, size_t maxSends ) 
+--			This function constructs a packet for the Grapefruit protocol.  The packet has protocol compliant control
+--			characters, has up to 1018 bytes of data, padded with ETX characters if there is less data.  It then has
+--			a 32 bit CRC appended to the last four bytes in setCRC();
+-- RETURNS: void
+--
+-- NOTES:
+----------------------------------------------------------------------------------------------------------------------*/
+void constructPacket( unsigned char* packet, size_t maxSends ) 
+{
+	//should check this at a higher level, so they know
+	if( maxSends >= MAX_SENDS )
+		return;
+		
+	if( buffer.size() > MAX_DATA )
+	{
+		packet[0] = ETB;
+		packet[1] = syncSend;
+		
+		for( size_t i = 0; i < MAX_DATA; i++ )
+		{
+			packet[i + 2] = (unsigned char)buffer[i];
+		}
+	}
+	else
+	{
+		packet[0] = EOT;
+		packet[1] = syncSend;
+		size_t i = 0;
+		for(; i < buffer.size(); i++ )
+		{
+			packet[i + 2] = buffer[i];
+		}
+		for(; i < MAX_DATA; i++)
+		{
+			packet[i + 2] = ETX;
+		}
+	}
+	
+	syncSend = ( syncSend == SYN1 ) ? SYN2 : SYN1;
+	setCRC( packet );
+	
+	return;
+}
+
+/*------------------------------------------------------------------------------------------------------------------
+-- FUNCTION: setCRC
+--
+-- DATE: November 18, 2014
+--
+-- REVISIONS: (Date and Description)
+--
+-- DESIGNER: Jeff Bayntun
+--
+-- PROGRAMMER: Jeff Bayntun
+--
+-- INTERFACE: void setCRC( unsigned char* packet ) 
+--			This function makes a 32 bit CRC for the message portion ( starting at packet[2] to packet[1020]
+--
+-- RETURNS: void
+--
+-- NOTES:
+----------------------------------------------------------------------------------------------------------------------*/
+void setCRC( unsigned char* packet )
+{
+	crc c = crcFast( &packet[2], MAX_DATA );
+	
+	unsigned char* crcArray = new unsigned char[4];
+	crcArray = reinterpret_cast<unsigned char *>(&c);	
+
+	for(size_t i = 0; i < 4; i++)
+	{
+		packet[ MAX_DATA + 2 + i ] = crcArray[i];
+	}
+
+	return;
+}
+
+/*------------------------------------------------------------------------------------------------------------------
+-- FUNCTION: trimResponse
+--
+-- DATE: November 18, 2014
+--
+-- REVISIONS: (Date and Description)
+--
+-- DESIGNER: Jeff Bayntun
+--
+-- PROGRAMMER: Jeff Bayntun
+--
+-- INTERFACE: string constructPacket( unsigned char* response ) 
+--			This function takes a Grapefruit Protocol packet.  This packet is assumed to be pre-validated, meaning
+--			that it has passed CRC validation has been confirmed to have the correct EOT or ETB prefix, and has the
+--			appropriate sync byte in as its second character.  It returns a string made up of the data in the packet,
+--			either to ETX, or the full 1018 bytes of data.
+-- RETURNS: string containing all data in the packet.
+--
+-- NOTES:
+----------------------------------------------------------------------------------------------------------------------*/
+
+std::string trimResponse( unsigned char* response )
+{
+	std::string data;
+	data.reserve(MAX_DATA);
+	
+	data = "";
+	for( size_t i = 2; response[i] != ETX && i < MAX_DATA + 2; i++)
+	{
+		data += response[i];
+	}
+	data.shrink_to_fit();
+
+	return data;
+}
+
+/*------------------------------------------------------------------------------------------------------------------
+-- FUNCTION: validateCRC
+--
+-- DATE: November 18, 2014
+--
+-- REVISIONS: (Date and Description)
+--
+-- DESIGNER: Jeff Bayntun
+--
+-- PROGRAMMER: Jeff Bayntun
+--
+-- INTERFACE: bool setCRC( unsigned char* response ) 
+--			calculates the 32 bit crc for the data portion of the response (response[2] to response[1020])
+--			and compares it to the crc received with the response.  If they are equal it returs true else false.
+--
+-- RETURNS: true if recieved CRC is valid
+--
+-- NOTES:
+----------------------------------------------------------------------------------------------------------------------*/
+bool validateCRC( unsigned char* response )
+{
+	crc* c = reinterpret_cast<crc *>( &response[ MAX_DATA + 2 ] );
+	
+	return *c == crcFast( &response[2], MAX_DATA );
+}
