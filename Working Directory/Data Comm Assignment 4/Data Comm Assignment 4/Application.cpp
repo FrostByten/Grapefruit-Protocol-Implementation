c228c372ab87b1e05a85a4642c6ed6d9baddeed0
jbayntun <j_bayntun@hotmail.com>
2014-12-02 21:02:17 -0800

    poor mans scrolling

--- Working Directory/Data Comm Assignment 4/Data Comm Assignment 4/Application.cpp
+++ Working Directory/Data Comm Assignment 4/Data Comm Assignment 4/Application.cpp
@@ -12,6 +12,8 @@
 --		void printDebugString(char* str);
 --		void addToTotalMessage();
 --		void displayReceived();
+--		void updateTotalMessages();
+--		void clearData();
 --
 --
 -- DATE: November 15, 2014
@@ -765,7 +767,7 @@ void displayReceived()
 --
 -- DESIGNER: Chris Klassen
 --
--- PROGRAMMER: Chris Klassen
+-- PROGRAMMER: Chris Klassen, Jeff Bayntun
 --
 -- INTERFACE: void updateWrapLength();
 --
@@ -831,6 +833,8 @@ void updateWrapLength()
 		totalMessage.push_back(message);
 	}
 
+	updateTotalMessages();
+
 	return;
 }
 
@@ -856,13 +860,7 @@ void updateWrapLength()
 ----------------------------------------------------------------------------------------------------------------------*/
 void addToTotalMessage()
 {
-	//string test = "First, buying real estate without an adequate down payment means extreme leverage, which brings added risk. No, houses do not go up forever. Therefore, buying with 5% down and twenty-times leverage means a lowly 10% correction in the market would wipe out all your savings. You’d owe more than the property’s worth. If you don’t think that’s possible, come back this time next year. We’ll talk.\n\nThen there’s the CMHC premium, which is north of 3% of the mortgaged amount for a minimal down payment. On a $500,000 mortgage, the cost is almost $16,000. Most people add this to their mortgage, so it gets amortized and effectively doubled over time. Your property then needs to appreciate an extra $30,000 just to break even. Bummer.\n\nIn short, a big down is good. This woman gets it. But what sense does it make to keep $200,000 in the tangerine guy’s shorts at 1.3%, when the inflation rate is 2.03% and all of the interest is taxable at your marginal rate? And what if house prices in your hood rise by 5% while you’re saving?\nRight. Fail. That money is actually shrinking due to inflation and taxes while you sit on it. Even at double or triple the interest rate, you’re falling further behind. And if you lock the cash into a five-year GIC to boost the return to 2.8% at some godforsaken online Lithuanian steelworkers’ benevolent Manitoba credit union, it’s not available to you should a great house deal materialize in 20 months from now.";
 	string message(printText);
-
-	//totalMessage.push_back("this is no1");
-	//totalMessage.push_back("this is no2");
-	//totalMessage.push_back("this is no3");
-
 	int width = analyticsDivider / 9;
 
 	while (message.length() > width)
@@ -886,6 +884,8 @@ void addToTotalMessage()
 	{
 		totalMessage.push_back(message);
 	}
+	updateTotalMessages();
+
 	printText[0] = '\0';
 }
 
@@ -914,4 +914,34 @@ void clearData()
 	refreshScreen();
 
 	return;
+}
+
+/*------------------------------------------------------------------------------------------------------------------
+-- FUNCTION: updateTotalMessages
+--
+-- DATE: Dec 2, 2014
+--
+-- REVISIONS: (Date and Description)
+--
+-- DESIGNER: Jeff Bayntun
+--
+-- PROGRAMMER: Jeff Bayntun
+--
+-- INTERFACE: void updateTotalMessages();
+--
+-- RETURNS: void
+--
+-- NOTES:
+-- Removes lines from output based on screen size so that most current output is visible.
+----------------------------------------------------------------------------------------------------------------------*/
+void updateTotalMessages()
+{
+	RECT rect;
+	GetClientRect(hwnd, &rect);
+
+	int height = (rect.bottom - rect.top) / 16;
+
+	while(totalMessage.size() > height - 3 )
+		totalMessage.erase( totalMessage.begin() );
+
 }
